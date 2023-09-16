5519ae06d1f19d136eee500c65769669e03a44e9
Alex-Wu-AU <alex.nanwu@gmail.com>
2023-09-17 01:06:43 +1000

    Registration Form Application using Vue components

---
+++ week7/my-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <RegistrationForm @user-registered="onUserRegistered" />
+    <GreetingMessage :userData="registeredUser" />
+  </div>
+</template>
+
+<script>
+import RegistrationForm from "./components/RegistrationForm.vue";
+import GreetingMessage from "./components/GreetingMessage.vue";
+
+export default {
+  components: {
+    RegistrationForm,
+    GreetingMessage,
+  },
+  data() {
+    return {
+      registeredUser: null,
+    };
+  },
+  methods: {
+    onUserRegistered(userData) {
+      this.registeredUser = userData;
+    },
+  },
+};
+</script>
---
+++ week7/my-app/src/components/GreetingMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ greetingMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userData: Object,
+  },
+
+  computed: {
+    greetingMessage() {
+      if (this.userData) {
+        return `Welcome, ${this.userData.name}! Thank you for registering on our shopping`;
+      }
+      return "";
+    },
+  },
+};
+</script>
